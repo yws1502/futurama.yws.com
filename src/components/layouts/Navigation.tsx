ab1ae867ff96382239cc78751d1557618dbcaefe
Woosang <yws1502@naver.com>
2021-12-28 00:32:06 +0900

    💄 Navigation styled

--- src/components/layouts/Navigation.tsx
+++ src/components/layouts/Navigation.tsx
@@ -1,13 +1,18 @@
 import styled from "@emotion/styled";
 import Link from "next/link"
 import { MEDIA_QUERY_END_POINT } from "../../constants";
+import { API_ENDPOINT } from "../../constants";
 
 export const Navigation = () => {
   const routerList = ["info", "characters", "cast", "episodes", "questions", "inventory"];
   return (
     <Header>
       <Inner>
-        <Logo>Futuerama</Logo>
+        <Logo className="test">
+          <Link href={'/'}>
+            <a>Futuerama</a>
+          </Link>
+        </Logo>
         <nav>
           <Menu>
             {routerList.map((router: string, index: number) => {
@@ -31,32 +36,37 @@ export const Navigation = () => {
 
 const Header = styled.header`
   box-shadow: 2px 2px 10px 2px rgba(0,0,0,0.2);
-  `
+  width: 100%;
+`
 
 const Inner = styled.div`
   display: flex;
   justify-content: space-between;
   max-width: 1280px;
-  margin: 0 auth;
+  margin: 0 auto;
+  padding: 0.7rem 0;
 `
 
 const Logo = styled.h1`
-  margin: 6px 0;
-  line-height: -10px;
-`
+  font-size: 28px;
+  font-weight: bold;
+  line-height: 42px;
+  `
 
 const Menu = styled.ul`
   display: flex;
   list-style: none;
-  font-size: 1.2rem;
-  margin: 0;
-`
+  font-size: 1.1rem;
+  `
 
 const ListItem = styled.li`
-  transition: .3s;
-  margin: 1rem;
-  
+  transition: .2s;
+  font-weight: bold;
+  margin: .7rem;
+  padding: 9px 10px;
+  border-radius: 50px;
   &:hover {
-    color: pink;
+    color: white;
+    background-color: orange;
   }
 `
