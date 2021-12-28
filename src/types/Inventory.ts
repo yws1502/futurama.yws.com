6534fed34e10d0245303bae06961215d594263d8
Woosang <yws1502@naver.com>
2021-12-28 17:09:16 +0900

    ✨ Create inventory type file

---
+++ src/types/Inventory.ts
@@ -0,0 +1,9 @@
+export interface Inventory {
+  title: string,
+  category: string,
+  description: string,
+  id: number,
+  price: number,
+  slogan: string,
+  stock: number,
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,4 @@ export * from "./Cast";
 export * from "./Creator";
 export * from "./Character";
 export * from "./Episodes";
+export * from "./Inventory";
