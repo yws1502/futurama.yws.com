89fd99e98c2888defe03d6b8c242cf38623ff3cf
Woosang <yws1502@naver.com>
2021-12-31 10:59:41 +0900

    🚚 파일 경로 정리

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,9 +1,5 @@
-export * from "./common/Error";
-export * from "./common/Loading";
-export * from "./common/PageTitle";
-export * from "./Character/CharacterCard";
-export * from "./Character/CharacterContainer";
-export * from "./Info/InfoContainer";
-export * from "./Cast/CastContainer";
-export * from "./Cast/CastCard";
-
+export * from "./Cast";
+export * from "./Character";
+export * from "./common";
+export * from "./Info";
+export * from "./layouts";
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.css';
 import "../styles/reset.css";
 import type { AppProps } from 'next/app';
-import { Layout } from "../components/layouts";
+import { Layout } from "../components";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
